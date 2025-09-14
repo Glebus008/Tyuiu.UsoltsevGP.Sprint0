ec5a86950cb8c5af5bc82b6c4b3d365d97992a18
Глеб Усольцев <glebus008@gmail.com>
2025-09-15 00:26:19 +0500

    Task2

---
+++ Tyuiu.UsoltsevGP.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.UsoltsevGP.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.UsoltsevGP.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Глеб";
+            var res = DataService.GetMessage(name);
+
+            // Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет..., Глеб", res);
+        }
+    }
+}
