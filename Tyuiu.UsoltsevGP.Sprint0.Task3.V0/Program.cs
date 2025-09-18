100e0c0199f93ba01006d28db46fd449df2f4b43
Глеб Усольцев <glebus008@gmail.com>
2025-09-18 22:46:24 +0500

    Task3

---
+++ Tyuiu.UsoltsevGP.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.UsoltsevGP.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.UsoltsevGP.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
